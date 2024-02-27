d5c641bd0dc9ad1f225a181dc171e7dbb0fc9c63
danaivach <danai.vacht@gmail.com>
2024-02-27 09:46:30 +0100

    fix(cast-generic) resolve warnings without suppressing

--- src/main/java/ch/unisg/ics/interactions/hmas/bindings/BindingNotFoundException.java
+++ src/main/java/ch/unisg/ics/interactions/hmas/bindings/BindingNotFoundException.java
@@ -1,9 +1,5 @@
 package ch.unisg.ics.interactions.hmas.bindings;
 
-/**
- * Exception thrown whenever {@link ProtocolBindings#getBinding(Form)} is called
- * but no suitable binding has been registered for the URI scheme provided in the form.
- */
 public class BindingNotFoundException extends RuntimeException {
 
 }
